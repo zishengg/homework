4038c85581faa884858cf371cdf54a0e1195a2f6
zishengg <129036052+zishengg@users.noreply.github.com>
2023-05-30 21:50:50 +0800

    lab 4

--- stock replenishment .py
+++ stock replenishment .py
@@ -1,11 +1,18 @@
-def delete_item(inventory, item_code):
-    for item in inventory:
-        if item_code == item['code']:
-            inventory.remove(item)
-            print('Item has been deleted from the inventory.')
-            
-        else:
-            print("Item cannot be found from inventory.")
+def delete_item():
+    DeleteCode = int(input("Enter the item code for the list you wish to remove: "))
+    for item in GroceryList:
+        if DeleteCode in item:
+            delete_confirmation = input("Are you sure?\tYes/No: ")
+            if delete_confirmation == "Yes":
+                GroceryList.remove(item)
+                print('Item has been deleted from the inventory.')
+            if delete_confirmation == "No":
+                print("Removal cancelled")
+
+
+delete_item()
+print(GroceryList)
+
 
 def stock_replenishment(inventory, item_code, quantity):
     for item in inventory:
@@ -15,5 +22,5 @@ def stock_replenishment(inventory, item_code, quantity):
         
         else:
             print("Item cannot be found from inventory.")
-            
+
             
