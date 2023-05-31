0df5d3b8d5db918107045bec0161d0a5b7727b7a
zishengg <129036052+zishengg@users.noreply.github.com>
2023-05-31 18:30:39 +0800

    tweakin

--- lab 4 5.py
+++ lab 4 5.py
@@ -1,52 +1,51 @@
+import math
+
 def calculate_diameter(radius):
     return 2 * radius
 
-def calculate_circumference (radius):
-    pi = 3.14159
-    return 2 * pi * radius
+def calculate_circumference(radius):
+    return 2 * math.pi * radius
 
 def calculate_area(radius):
-    pi = 3.14159
-    return pi * radius **2
-
+    return math.pi * radius ** 2
 
 def display_menu():
     print('MENU')
-    print('1. Calulate Diamater')
-    print('2. Calulate Circumference')
-    print('3. Calulate Area')
-    
-def function():
+    print('1. Calculate Diameter')
+    print('2. Calculate Circumference')
+    print('3. Calculate Area')
+
+def main():
     display_menu()
-    choice = int(input('Please enter the option number'))
+    choice = int(input('Please enter the option number: '))
 
     if choice == 1:
-        try:   
-            radius = float(input("Enter radius"))
+        try:
+            radius = float(input("Enter radius: "))
             diameter = calculate_diameter(radius)
-            print ('The diamater of the circle is', diameter)
-        
+            print('The diameter of the circle is', diameter)
         except ValueError:
             print('error')
 
     elif choice == 2:
-        try: 
-            radius = float(input('Enter radius'))
+        try:
+            radius = float(input('Enter radius: '))
             circumference = calculate_circumference(radius)
-            print('The circumeference is', circumference)
-        
+            print('The circumference is', circumference)
         except ValueError:
-            print('Error')
+            print('error')
 
     elif choice == 3:
         try:
-            radius = float(input('Enter radius'))
+            radius = float(input('Enter radius: '))
             area = calculate_area(radius)
             print('The area is', area)
         except ValueError:
-            print('Error')
+            print('error')
 
     else:
-        print('Error')
+        print('error')
 
+if __name__ == "__main__":
+    main()
 
