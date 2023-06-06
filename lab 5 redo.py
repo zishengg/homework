8d0931ca9e69e79af51e7243d08f49d8db1a1110
zishengg <129036052+zishengg@users.noreply.github.com>
2023-06-06 14:01:45 +0800

    ds

--- lab 5 redo.py
+++ lab 5 redo.py
@@ -1,64 +1,58 @@
-def user_input():
-    print("Do you want to continue? '0' to Continue '-1' to Terminate:")
-    choice = int(input("Continue or terminate?"))
-    if choice == 0:
-        option = int(input("Your Options are:\n\t1. Add new student detail.\n\t2. View all student details.\n\t3. Search Specific student detail."))
-        if option == 1:
-            student = []
-            name = input("Enter Student Name: ")
-            tpnumber = input(f"Enter {name}'s TP Number: ")
-            subcount = int(input("How many subjects in Semester: "))
+def display(total, average):
+    print(f"Total of all Subjects: {total}\nAverage of Semester: {average}")
+    print("Grade of Semester: ", grade(average))
 
-            marks = []
-            for i in range(subcount):
-                subject_marks = int(input("Enter " + str(i+1) + " subject Marks: "))
-                marks.append(subject_marks)
-
-            student.append(name)
-            student.append(tpnumber)
-            student.append(subcount)
-    elif option == 2:
-        print("Not specified yet")
-    elif option == 3:
-        print("Not specified yet")
-    else:
-        print("Error")
-
-def display():
-    display_name = student[0]
-    display_tp = student[1]
-    display_total = mark
-    display_percentage = average
-    display_grade = grade
-    print("Name: ", display_name)
-    print("TP Number: ",display_tp)
-    print("Total: ",display_total)
-    print("Percentage: ",display_percentage ,"%")
-    print("Grade: ", display_grade)
-    return 
-
-
-def mark(marks):
+def mark(marks:list):
     return sum(marks)
 
-def average():
-    average = mark / subcount
-    return average
+def avg(total:int, num:int):
+    return total/num
 
 def grade(average):
-    if average >= 80:
-        return "A+"
-    elif average >= 75:
-        return "A"
-    elif average >= 70:
-        return "B+"
-    elif average >= 65:
-        return "B"
-    elif average >= 60:
-        return "C+"
-    elif average >= 55:
-        return "C"
-    elif average >= 50:
-        return "C-"
+    g=""
+    if average >=80:
+        g="A+"
+    elif average>=75:
+        g="A"
+    elif average>=70:
+        g="B+"
+    elif average>=65:
+        g="B"
+    elif average>=60:
+        g="C+"
+    elif average>=55:
+        g="C"
+    elif average>=50:
+        g="C-"
     else:
-        return "D/Fail"
+        g="D/Fail"
+    return g
+
+
+data_ls=[]
+while True:
+    tmp=[]    
+    opt=input("Do you want to continue '0' to Continue '-1' to Terminate :")
+    choice=name=tp=num_subject=""
+
+    if opt=="-1":
+        break
+    elif opt=="0":
+        print("Your options are:")
+        print("1. Add new student detail.\n2. View all student details\n3. Search Specific Student Detail.\n")
+        choice=input("Select your choice: ")
+        match (choice):
+            case "1":
+                name=input("Enter Student Name: ")
+                tp=input(f"Enter {name}'s TP Number: ")
+                num_subject=int(input("How many subjects in Semester: "))
+                tmp=[name, tp]
+                for i in range(num_subject):
+                    tmp.append(int(input(f"Enter {i+1} subject Marks: ")))
+                total=mark(tmp[3:])
+                average=avg(total, num_subject)
+                display(total, average)
+                data_ls.append(tmp)
+                
+    else:
+        print("Invalid option")
